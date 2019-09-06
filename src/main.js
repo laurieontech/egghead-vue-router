37ec1df35e9e698149075dbea8cbed12a6b81ee7
laurie <laurie@tenmilesquare.com>
2019-09-06 13:57:43 -0400

    first route defined

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+import Home from './components/Home.vue'
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    //show home path, only one of these can exist
+    path: '/',
+    component: Home,
+  },
+]
 
 const router = new VueRouter({
   routes,
