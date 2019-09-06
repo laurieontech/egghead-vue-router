0ca492da6e77438b7c3487ba16a2ddd77dd9d8bb
laurie <laurie@tenmilesquare.com>
2019-09-06 13:55:09 -0400

    install and set up router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = []
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: (h) => h(App),
 }).$mount('#app')
